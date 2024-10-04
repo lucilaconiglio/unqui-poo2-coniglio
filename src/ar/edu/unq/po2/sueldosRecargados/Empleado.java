964e97f117063d5860318a781a6a636e19367c9e
lucila <75704088+lucilaconiglio@users.noreply.github.com>
2024-10-03 23:21:49 -0300

    le agregue el final al metodo template

--- src/ar/edu/unq/po2/sueldosRecargados/Empleado.java
+++ src/ar/edu/unq/po2/sueldosRecargados/Empleado.java
@@ -4,7 +4,7 @@ public abstract class Empleado {
 			
 	abstract double calcularSueldoBruto();
 
-	protected double sueldo() {
+	public final double sueldo() {
 	    return calcularSueldoBruto() * 0.87;  // 87% del sueldo bruto, ya descontado el 13%
 	}
 
